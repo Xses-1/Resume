d24196eebaf6f783d71c720aada4466999196e46
xses <wiktort52@gmail.com>
2023-07-05 22:48:29 +0200

    Progress on main.tex. New compilation command. Modifications to theme.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # kill -15 $(pgrep firefox)
-pdflatex *.tex
+: | pdflatex -halt-on-error *.tex | grep '^!.*' -A200 --color=always
 # firefox /home/xses/Desktop/Resume Wiktor Tomanek-1.pdf *.pdf &
